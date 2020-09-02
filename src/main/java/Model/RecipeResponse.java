f1a91765626a4dde747b0a95e96b0ac1af0d9252
syedfaizanalam <faizan92alam@gmail.com>
2020-09-02 16:53:59 -0400

    kumite

--- src/main/java/Model/RecipeResponse.java
+++ src/main/java/Model/RecipeResponse.java
@@ -2,7 +2,7 @@ package Model;
 
 import java.util.List;
 
-public class RecipeReponse {
+public class RecipeResponse {
 
 
 	private String q;
@@ -10,13 +10,8 @@ public class RecipeReponse {
 	
 	
 	
-	public RecipeReponse() {
+	public RecipeResponse() {
 		super();
-	}
-	public RecipeReponse(String q, List<Diet> hits) {
-		super();
-		this.q = q;
-		this.hits = hits;
 	}
 	public String getQ() {
 		return q;
