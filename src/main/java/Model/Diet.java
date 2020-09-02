f1b1044fbf2e8acee1330ae8e0badca9191e5d9c
syedfaizanalam <faizan92alam@gmail.com>
2020-09-02 16:55:58 -0400

    ohayo

--- src/main/java/Model/Diet.java
+++ src/main/java/Model/Diet.java
@@ -2,7 +2,24 @@ package Model;
 
 public class Diet {
 
-	private RecipeResponse recipe; 
+	private RecipeResponse recipe;
+
+	public Diet() {
+		super();
+	}
+
+	public RecipeResponse getRecipe() {
+		return recipe;
+	}
+
+	public void setRecipe(RecipeResponse recipe) {
+		this.recipe = recipe;
+	}
+
+	@Override
+	public String toString() {
+		return "Diet [recipe=" + recipe + "]";
+	} 
 	
 	
 	
