dee0c4a8c3c8a8e20ab51488c236229e1b538180
syedfaizanalam <faizan92alam@gmail.com>
2020-09-03 09:52:59 -0400

    commit

--- src/main/java/Model/Diet.java
+++ src/main/java/Model/Diet.java
@@ -24,20 +24,10 @@ public class Diet {
 	}
 
 
-
-	private RecipeResponse recipe;
-
 	public Diet() {
 		super();
 	}
 
-	public RecipeResponse getRecipe() {
-		return recipe;
-	}
-
-	public void setRecipe(RecipeResponse recipe) {
-		this.recipe = recipe;
-	}
 
 	@Override
 	public String toString() {
--- src/main/java/Model/RecipeResponse.java
+++ src/main/java/Model/RecipeResponse.java
@@ -8,22 +8,19 @@ public class RecipeResponse {
 	private String q;
 	private List<Diet> hits;
 	
-<<<<<<< HEAD:src/main/java/Model/RecipeResponse.java
 	
 	
 	public RecipeResponse() {
 		super();
 	}
-=======
-	public RecipeResponse() {
-		super();
-	}
+
+
 	public RecipeResponse(String q, List<Diet> hits) {
 		super();
 		this.q = q;
 		this.hits = hits;
 	}
->>>>>>> 731269096917551cb0cac4fcf4d3b340388daac0:src/main/java/Model/RecipeReponse.java
+	
 	public String getQ() {
 		return q;
 	}
